9825de8d69f7716e5049c686d514e34b00f06711
AnaComBia <anabeatrizetorres@gmail.com>
2021-08-21 14:38:21 -0300

    Finaliza Progama Musica

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Musica_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White; 
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine();
+            Console.WriteLine("Admiravel Chip Novo\nPitty");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Pane no sistema\nAlguém me desconfigurou\nAonde estão meus olhos de robô?\nEu não sabia, eu não tinha percebido\nEu sempre achei que era vivo");
+            Console.WriteLine();
+            Console.WriteLine("Parafuso e fluido em lugar de articulação\nAté achava que aqui batia um coraçãoNada é orgânico, é tudo programado\nE eu achando que tinha me libertado");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Mas lá vem eles novamente\nEu sei o que vão fazer\nReinstalar o sistema");
+            Console.WriteLine();
+            Console.WriteLine("Pense, fale, compre, beba\nLeia, vote, não se esqueça\nUse, seja, ouça, diga\nTenha, more, gaste e viva");
+            Console.WriteLine("Pense, fale, compre, beba\nLeia, vote, não se esqueça\nUse, seja, ouça, diga");
+            Console.WriteLine();
+            Console.WriteLine("Não, senhor\nSim, senhor\nNão, senhor\nSim, senhor");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Pane no sistema\nAlguém me desconfigurou\nAonde estão meus olhos de robô?\nEu não sabia, eu não tinha percebido\nEu sempre achei que era vivo");
+            Console.WriteLine("Parafuso e fluido em lugar de articulação\nAté achava que aqui batia um coraçãoNada é orgânico, é tudo programado\nE eu achando que tinha me libertado");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Mas lá vem eles novamente\nEu sei o que vão fazer\nReinstalar o sistema");
+            Console.WriteLine();
+            Console.WriteLine("Pense, fale, compre, beba\nLeia, vote, não se esqueça\nUse, seja, ouça, diga\nTenha, more, gaste e viva");
+            Console.WriteLine("Pense, fale, compre, beba\nLeia, vote, não se esqueça\nUse, seja, ouça, diga");
+            Console.WriteLine();
+            Console.WriteLine("Não, senhor\nSim, senhor\nNão, senhor\nSim, senhor");
+            Console.WriteLine("Mas lá vem eles novamente\nEu sei o que vão fazer\nReinstalar o sistema");
+
+
+             Console.ResetColor();
+        }
+    }
+}
+
